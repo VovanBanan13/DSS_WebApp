b485588a7815dc39b87eaa2e49979e1dd2015d2c
VovanBanan13 <bananvovan@gmail.com>
2021-04-28 16:21:32 +0300

    Add table in BD

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import Region
+from app.models import Region, Material
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Region': Region}
+    return {'db': db, 'Region': Region, 'Material' : Material}
--- app/models.py
+++ app/models.py
@@ -4,6 +4,17 @@ class Region(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     temperature = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Region {}>'.format(self.name)
+
+class Material(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    thermal = db.Column(db.Integer)
+    depth = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Material {}>'.format(self.name)
