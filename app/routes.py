d5480b31daf95cfbc0f165edee1e909ef829f2dc
VovanBanan13 <bananvovan@gmail.com>
2021-05-13 13:18:18 +0300

    Выпадающие списки

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from app.models import Region, Material
 
 @app.route('/')
 @app.route('/index')
 def index():
-    list = ["Республика Мордовия", "Московская область", "Пензенская область","Краснодарский край"]
-    return render_template('index.html', option=list)
+    regions_obj = Region.query.all()
+    regions = [x.name for x in regions_obj]
+
+    materials_obj = Material.query.all()
+    materials = [x.name for x in materials_obj]
+
+    data = {
+        'region' : regions,
+        'material' : materials
+    }
+
+    return render_template('index.html', regions=regions, materials=materials, data=data)
