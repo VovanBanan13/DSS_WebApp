bf1a6f83a7c3c241ff29dbe08384b835539db422
VovanBanan13 <bananvovan@gmail.com>
2021-05-17 18:19:53 +0300

    Добавил кнопку на форму

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,16 @@ from wtforms import StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from app.models import Region, Material
 
-regions_obj = Region.query.all()
-regions = [x.name for x in regions_obj]
+# regions_obj = Region.query.all()
+# regions = [x.name for x in regions_obj]
 
-materials_obj = Material.query.all()
-materials = [x.name for x in materials_obj]
+# materials_obj = Material.query.all()
+# materials = [x.name for x in materials_obj]
 
-data = {
-    'region' : regions,
-    'material' : materials
-}
+# data = {
+#     'region' : regions,
+#     'material' : materials
+# }
 
 class Choiсe(FlaskForm):
     regions_obj = Region.query.all()
@@ -22,9 +22,6 @@ class Choiсe(FlaskForm):
     materials_obj = Material.query.all()
     materials = [x.name for x in materials_obj]
 
-    data = {
-        'region' : regions,
-        'material' : materials
-    }
     region = SelectField(u'Выберите регион:',choices=[(r, r) for r in regions])
     material = SelectField(u'Выберите материал:', choices=[(m, m) for m in materials])
+    submit = SubmitField('Рассчитать')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
+from flask.helpers import flash, url_for
 from app import app
 from app.models import Region, Material
 from app.forms import Choiсe
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     regions_obj = Region.query.all()
     regions = [x.name for x in regions_obj]
@@ -13,9 +14,11 @@ def index():
     materials_obj = Material.query.all()
     materials = [x.name for x in materials_obj]
 
-    data = {
-        'region' : regions,
-        'material' : materials
-    }
     form = Choiсe()
-    return render_template('index.html', regions=regions, materials=materials, data=data, form=form)
+    if form.validate_on_submit():
+        flash('Расчёт произведён, результаты смотрите ниже. Выбран регион: {}, материалы: {}'.format(form.region.data, form.material.data))
+        # flash('Расчёт произведён, результаты смотрите ниже.')
+        # return redirect('/')
+        return redirect(url_for('index'))
+    return render_template('index.html', regions=regions, materials=materials, form=form)
+    # return render_template('index.html', form=form)
