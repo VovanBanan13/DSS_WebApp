1a7747ab7f7a8982db6ab413782b94d1b8c5207c
VovanBanan13 <bananvovan@gmail.com>
2021-06-18 23:50:48 +0300

    Heroku v3

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,9 @@
 from app import app, db
 from app.models import Region, Material
+from app import getApp
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Region': Region, 'Material' : Material}
+
+app = getApp()
